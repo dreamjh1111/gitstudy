ea9ceef2165d81a08fe2004c99b782e35d732ee9
dreamjh1111 <dreamjh1111@gmail.com>
2021-10-20 09:35:26 +0900

    shell script added

---
+++ gitsetting.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git init
+git config --global user.email "dreamjh1111@gmail.com"
+git config --global user.name "dreamjh1111"
+vim readme.md
+git add readme.md
+git commit -m "first commit"
